d7c65d77a6a255c8e0379f53752352d6518c5f78
nthlua <nthlyaraissa@gmail.com>
2018-02-26 17:11:14 -0300

    oi

--- Projeto/src/Projeto/Aluno.java
+++ Projeto/src/Projeto/Aluno.java
@@ -1,19 +1,18 @@
 package Projeto;
 
 public class Aluno {
-
-	private String matricula;
+	protected String matricula;
 	private String nome;
 	private int codigoCurso;
 	private String email;
-	private int nota;
-	private String telefone;
-
+	private int nota = 5;
+	private String telefone;	
+	
 	public Aluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
 		if (nome.trim().equals("")) {
 			throw new NullPointerException("Erro no cadastro de aluno: Nome nao pode ser vazio ou nulo");
 		}
-		if (email.indexOf("@") == 0 || email.indexOf("@") == email.length() - 1) {
+		if (email.indexOf("@") == 0 || email.indexOf("@") == email.length()-1 || !email.contains("@")) {
 			throw new IllegalArgumentException("Erro no cadastro de aluno: Email invalido");
 		}
 		this.matricula = matricula;
@@ -21,15 +20,22 @@ public class Aluno {
 		this.codigoCurso = codigoCurso;
 		this.telefone = telefone;
 		this.email = email;
-		this.nota = 5;
 	}
 
 	public String getMatricula() {
 		return matricula;
 	}
 
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
 	public String getNome() {
 		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public int getNota() {
@@ -44,12 +50,33 @@ public class Aluno {
 		return codigoCurso;
 	}
 
+	public void setCodigoCurso(int codigoCurso) {
+		this.codigoCurso = codigoCurso;
+	}
+
 	public String getEmail() {
 		return email;
 	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	public String getTelefone() {
 		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public String toString() {
+		if (telefone.trim().equals("")) {
+		return this.matricula + " - " + this.nome + " - " +  this.codigoCurso + " - " + this.email;
+	}
+		return this.matricula + " - " +  this.nome + " - " + this.codigoCurso + " - " + this.telefone + " - " + this.email;
+		
 	}
 
 	@Override
@@ -76,14 +103,6 @@ public class Aluno {
 			return false;
 		return true;
 	}
-
-	@Override
-	public String toString() {
-		if (telefone.trim().equals("")) {
-			return this.matricula + " - " + this.nome + " - " + this.codigoCurso + " - " + this.email;
-		}
-		return this.matricula + " - " + this.nome + " - " + this.codigoCurso + " - " + this.telefone + " - "
-				+ this.email;
-
-	}
+	
+	
 }
--- Projeto/src/Projeto/Tutor.java
+++ Projeto/src/Projeto/Tutor.java
@@ -1,11 +1,15 @@
 package Projeto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tutor extends Aluno {
 
 	private String disciplina;
 	private int proficiencia;
 	private int nota;
 	private int dinheiro;
+	public List<String> disciplinas;
 
 	public Tutor(String nome, String matricula, int codigoCurso, String telefone, String email, String disciplina,
 			int proficiencia) {
@@ -16,6 +20,21 @@ public class Tutor extends Aluno {
 		this.proficiencia = proficiencia;
 		this.nota = 4;
 		this.dinheiro = 0;
+		this.disciplinas = new ArrayList<>();
+	}
+
+	public void disciplinasTutor(String disciplina) {
+		disciplinas.add(disciplina);
+	}
+
+	public boolean verificaDisciplinas(String disciplina) {
+		boolean aux = false;
+		for (String disc : disciplinas) {
+			if (disc.equals(disciplina)) {
+				aux = true;
+			}
+		}
+		return aux;
 	}
 
 	public int getProficiencia() {
@@ -41,16 +60,44 @@ public class Tutor extends Aluno {
 	public void setDinheiro(int dinheiro) {
 		this.dinheiro = dinheiro;
 	}
-	
+
+	public String getMatricula() {
+		return matricula;
+	}
+
 	public String getDisciplina() {
 		return disciplina;
 	}
-	
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return super.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((disciplina == null) ? 0 : disciplina.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tutor other = (Tutor) obj;
+		if (disciplina == null) {
+			if (other.disciplina != null)
+				return false;
+		} else if (!disciplina.equals(other.disciplina))
+			return false;
+		return true;
 	}
 
 }
