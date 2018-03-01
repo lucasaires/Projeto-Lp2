c168f615ef911695ff8586d621cc6bacd22b75bd
nthlua <nthlyaraissa@gmail.com>
2018-03-01 12:02:10 -0300

    Tutores e dor de cabeça

--- Projeto/src/Nathalya/AjudaOnline.java
+++ Projeto/src/Nathalya/AjudaOnline.java
@@ -1,14 +1,13 @@
 package Nathalya;
 
-public class AjudaOnline implements Ajuda  {
+public class AjudaOnline  {
 	private String disciplina;
 	private int id;
-	private String tutor;
+
 	
-	public AjudaOnline(String disciplina,int id,String tutor) {
+	public AjudaOnline(String disciplina,int id) {
 		this.disciplina = disciplina;
 		this.id = id;
-		this.tutor = tutor;
 	}
 
 	public String getDisciplina() {
@@ -18,10 +17,5 @@ public class AjudaOnline implements Ajuda  {
 	public int getId() {
 		return id;
 	}
-
-	public String getTutor() {
-		return tutor;
-	}
-	
 	
 }
